3902566852d1121431e80ea7e088f59d26772bd6
bakon <bakon@pipe.kitchen>
2024-12-18 23:45:15 -0600

    small add ons

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,10 +2,21 @@ import './assets/main.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { ReusableProvider } from 'reusable'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import App from './containers/App'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const routing = (
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </Router>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <ReusableProvider>
+    <React.StrictMode>{routing}</React.StrictMode>
+  </ReusableProvider>
 )
